980f5f369c85fc3cf6c32328331c3e2bc4b393b5
kyle.grenell@gmail.com <kyle.grenell@gmail.com>
2017-01-06 20:26:10 +0000

    get api genre root working with mongodb

--- mountaineer_productions/app.js
+++ mountaineer_productions/app.js
@@ -3,14 +3,25 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose =require('mongoose');
 
-// connect to mongoose
+Genre = require('./models/genre');
 
+// connect to mongoose
 mongoose.connect('mongodb://localhost/mountaineers');
 var db = mongoose.connection;
 
 // http requests - restful routes
 app.get('/', function(req, res){
   res.send('Please use /api/packs');
+});
+
+// genres
+app.get('/api/genres', function(req, res){
+  Genre.getGenres(function(err, genres){
+    if(err){
+      throw err;
+    }
+    res.json(genres);
+  });
 });
 
 //listen on port 3000
